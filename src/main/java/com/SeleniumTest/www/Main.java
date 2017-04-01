7be25a9090a5a9b0f43cc3e1e8137e8cb9648a6d
Min-ho Kim <minmin0416@naver.com>
2017-04-01 16:24:01 +0900

    First Commit in SeleniumTest

---
+++ src/main/java/com/SeleniumTest/www/Main.java
@@ -0,0 +1,41 @@
+package com.SeleniumTest.www;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by minmi on 2017-03-24.
+ */
+public class Main {
+    public static void main(String args[])
+    {
+        WebDriver driver;
+        System.setProperty("webdriver.gecko.driver", "D:\\My_Project\\geckodriver.exe");
+        driver =new FirefoxDriver();
+
+        driver.get("https://playoverwatch.com/ko-kr/game/patch-notes/pc/");
+
+        List<WebElement> posts  = driver.findElements(By.className("patch-notes-body"));
+        List<String> postsContents = new ArrayList<String>();
+
+        for(WebElement e : posts)
+        {
+            String tmp = e.getText();
+            postsContents.add(tmp);
+        }
+        for(String e : postsContents)
+        {
+            System.out.println("Page title is : " + e);
+        }
+        //Close the browser
+        driver.quit();
+
+    }
+}
